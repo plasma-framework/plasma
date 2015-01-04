e65fbde144cbdd1230da48e7e028e4af74810637
TerraMeta <scott-cinnamond@terrameta.org>
2015-01-04 09:25:15 -0700

    Update UMLDialect.java

--- plasma-sdo/src/main/java/org/plasma/provisioning/cli/UMLDialect.java
+++ plasma-sdo/src/main/java/org/plasma/provisioning/cli/UMLDialect.java
@@ -3,11 +3,22 @@ package org.plasma.provisioning.cli;
 /**
  * The UML editing tool dialect
  */
-public enum UMLDialect {
-    papyrus,
-    magicdraw;
+public enum UMLDialect implements OptionEnum {
+    papyrus("the Eclipse Papyrus specific UML dialect, particularly targeting the specifics related to UML profiles"),
+    magicdraw("the No Magic Inc. MagicDraw specific UML dialect, particularly targeting the specifics related to UML profiles");
     
-    public static String asString() {
+	private String description;
+
+	private UMLDialect(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String getDescription() {
+		return this.description;
+	}
+
+	public static String asString() {
 		StringBuilder buf = new StringBuilder();
 		for (int i = 0; i < UMLDialect.values().length; i++) {
 			if (i > 0)
