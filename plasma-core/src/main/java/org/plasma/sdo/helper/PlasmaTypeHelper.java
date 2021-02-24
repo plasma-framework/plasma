b42d2a8527c7c6bd6c61a8ecb66c9cf1129cf0b4
scinnamond <scinnamoond@gmail.com>
2021-02-24 13:23:09 -0700

    2.1.3 initial

--- plasma-core/src/main/java/org/plasma/sdo/helper/PlasmaTypeHelper.java
+++ plasma-core/src/main/java/org/plasma/sdo/helper/PlasmaTypeHelper.java
@@ -24,6 +24,7 @@ import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.plasma.runtime.ConfigurationException;
 import org.plasma.sdo.PlasmaDataObjectConstants;
 import org.plasma.sdo.PlasmaDataObjectException;
 import org.plasma.sdo.PlasmaType;
@@ -53,14 +54,162 @@ public class PlasmaTypeHelper implements TypeHelper {
   private Map<String, String> qualifiedAliasToLogicalNameMap = new HashMap<>();
 
   private PlasmaTypeHelper() {
-
-    this.namespaceQualifiedNameToTypeCache = new PlasmaCacheDelegate<PlasmaType>("plasmaTypeCache");
+    try {
+      this.namespaceQualifiedNameToTypeCache = new PlasmaCacheDelegate<PlasmaType>(
+          "plasmaTypeCache");
+    } catch (ConfigurationException ce) {
+      log.warn("no cache configuration found - continuing w/o cache");
+    }
   }
 
   private static synchronized PlasmaTypeHelper initializeInstance() {
     if (INSTANCE == null)
       INSTANCE = new PlasmaTypeHelper();
     return INSTANCE;
+  }
+
+  /**
+   * Return the Type specified by typeName with the given uri, or null if the
+   * underlying classifier is not found.
+   * 
+   * @param uri
+   *          the logical uri of the Type - See Type.getURI() or
+   *          PlasmaType.getURIPhysicalName();
+   * @param typeName
+   *          The logical name of the Type - Type.getName() or
+   *          PlasmaType.getPhysicalName();
+   * @return the Type specified by typeName with the given uri, or null if not
+   *         found.
+   */
+  public Type getType(String uri, String typeName) {
+    String qualifiedName = uri + "#" + typeName;
+    return getType(qualifiedName);
+  }
+
+  /**
+   * Return the Type specified by physical typeName with the given physical uri,
+   * or null if not found.
+   * 
+   * @param uriPhisicalName
+   *          the physical uri of the Type - See Type.getURI() or
+   *          PlasmaType.getURIPhysicalName();
+   * @param typeName
+   *          The logical or physical name of the Type - Type.getName() or
+   *          PlasmaType.getPhysicalName();
+   * @return the Type specified by typeName with the given uri, or null if not
+   *         found.
+   */
+  public Type findTypeByPhysicalName(String uriPhisicalName, String typePhysicalName) {
+    String qualifiedPhysicalName = uriPhisicalName + "#" + typePhysicalName;
+    return getType(qualifiedPhysicalName);
+  }
+
+  private Type getType(String qualifiedName) {
+    PlasmaType result = null;
+    if (namespaceQualifiedNameToTypeCache != null)
+      result = namespaceQualifiedNameToTypeCache.getObject(qualifiedName);
+    if (result == null) {
+      String qualifiedLogicalName = qualifiedName;
+      if (qualifiedAliasToLogicalNameMap.containsKey(qualifiedLogicalName))
+        qualifiedLogicalName = qualifiedAliasToLogicalNameMap.get(qualifiedLogicalName);
+      String[] tokens = qualifiedLogicalName.split("#");
+      try {
+        result = new CoreType(tokens[0], tokens[1]);
+      } catch (InvalidClassifierNameException e) {
+        log.warn(e.getMessage(), e);
+        return null;
+      }
+      if (namespaceQualifiedNameToTypeCache != null)
+        namespaceQualifiedNameToTypeCache.putObject(qualifiedName, result);
+      if (!result.getName().equals(tokens[1])) {
+        String aliasQualifiedName = tokens[0] + "#" + result.getName();
+        qualifiedAliasToLogicalNameMap.put(aliasQualifiedName, qualifiedLogicalName);
+      }
+      String qualifiedPhysicalName = findQualifiedPhysicalName(result);
+      if (qualifiedPhysicalName != null) {
+        qualifiedAliasToLogicalNameMap.put(qualifiedPhysicalName, qualifiedLogicalName);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Releases resources associated with the given type and removes it from
+   * cache.
+   * 
+   * @param type
+   *          the type
+   */
+  public void releaseType(Type type) {
+    releaseType(type.getURI(), type.getName());
+  }
+
+  /**
+   * Releases resources associated with the given type and removes it from
+   * cache.
+   * 
+   * @param uri
+   *          The logical uri of the Type - type.getURI();
+   * @param typeName
+   *          The logical name of the Type - type.getName();
+   */
+  public void releaseType(String uri, String typeName) {
+    String qualifiedName = uri + "#" + typeName;
+    if (log.isDebugEnabled())
+      log.debug("releasing type: " + qualifiedName);
+    if (namespaceQualifiedNameToTypeCache != null)
+      namespaceQualifiedNameToTypeCache.clearObject(qualifiedName);
+  }
+
+  /**
+   * Return the Type for this interfaceClass or null if not found. The
+   * interfaceClass is the Java interface that follows the SDO code generation
+   * pattern.
+   * 
+   * @param interfaceClass
+   *          is the interface for the DataObject's Type -
+   *          type.getInstanceClass();
+   * @return the Type for this interfaceClass or null if not found.
+   * @throws IllegalAccessException
+   * @throws IllegalArgumentException
+   */
+  public Type getType(Class interfaceClass) {
+    String uri = null;
+    try {
+      Field uriField = interfaceClass
+          .getDeclaredField(PlasmaDataObjectConstants.NAMESPACE_URI_FIELD_NAME);
+      uri = (String) uriField.get(null);
+    } catch (SecurityException e) {
+      throw new PlasmaDataObjectException(e);
+    } catch (NoSuchFieldException e) {
+      throw new PlasmaDataObjectException(e);
+    } catch (IllegalArgumentException e) {
+      throw new PlasmaDataObjectException(e);
+    } catch (IllegalAccessException e) {
+      throw new PlasmaDataObjectException(e);
+    }
+    return getType(uri, interfaceClass.getSimpleName());
+  }
+
+  /**
+   * Return the Type instances specified by the given uri.
+   * 
+   * @param uri
+   *          The namespace uri;
+   * @return the Type instances specified by the given uri.
+   */
+  public List<Type> getTypes(String uri) {
+    List<Type> result = new ArrayList<>();
+    List<Classifier> list = PlasmaRepository.getInstance().getClassifiers(uri);
+    for (Classifier classifier : list) {
+      if (classifier instanceof Stereotype) {
+        log.warn("ignoring stereotype: " + classifier.getName());
+        continue;
+      }
+      Type type = getType(uri, classifier.getName());
+      result.add(type);
+    }
+    return result;
   }
 
   /**
@@ -124,146 +273,6 @@ public class PlasmaTypeHelper implements TypeHelper {
   public Property getOpenContentProperty(String uri, String propertyName) {
     // TODO Auto-generated method stub
     throw new RuntimeException("not yet supported");
-  }
-
-  /**
-   * Return the Type specified by typeName with the given uri, or null if the
-   * underlying classifier is not found.
-   * 
-   * @param uri
-   *          the logical uri of the Type - See Type.getURI() or
-   *          PlasmaType.getURIPhysicalName();
-   * @param typeName
-   *          The logical name of the Type - Type.getName() or
-   *          PlasmaType.getPhysicalName();
-   * @return the Type specified by typeName with the given uri, or null if not
-   *         found.
-   */
-  public Type getType(String uri, String typeName) {
-    String qualifiedName = uri + "#" + typeName;
-    return getType(qualifiedName);
-  }
-
-  /**
-   * Return the Type specified by physical typeName with the given physical uri,
-   * or null if not found.
-   * 
-   * @param uriPhisicalName
-   *          the physical uri of the Type - See Type.getURI() or
-   *          PlasmaType.getURIPhysicalName();
-   * @param typeName
-   *          The logical or physical name of the Type - Type.getName() or
-   *          PlasmaType.getPhysicalName();
-   * @return the Type specified by typeName with the given uri, or null if not
-   *         found.
-   */
-  public Type findTypeByPhysicalName(String uriPhisicalName, String typePhysicalName) {
-    String qualifiedPhysicalName = uriPhisicalName + "#" + typePhysicalName;
-    return getType(qualifiedPhysicalName);
-  }
-
-  private Type getType(String qualifiedName) {
-    PlasmaType result = namespaceQualifiedNameToTypeCache.getObject(qualifiedName);
-    if (result == null) {
-      String qualifiedLogicalName = qualifiedName;
-      if (qualifiedAliasToLogicalNameMap.containsKey(qualifiedLogicalName))
-        qualifiedLogicalName = qualifiedAliasToLogicalNameMap.get(qualifiedLogicalName);
-      String[] tokens = qualifiedLogicalName.split("#");
-      try {
-        result = new CoreType(tokens[0], tokens[1]);
-      } catch (InvalidClassifierNameException e) {
-        log.warn(e.getMessage(), e);
-        return null;
-      }
-      namespaceQualifiedNameToTypeCache.putObject(qualifiedName, result);
-      if (!result.getName().equals(tokens[1])) {
-        String aliasQualifiedName = tokens[0] + "#" + result.getName();
-        qualifiedAliasToLogicalNameMap.put(aliasQualifiedName, qualifiedLogicalName);
-      }
-      String qualifiedPhysicalName = findQualifiedPhysicalName(result);
-      if (qualifiedPhysicalName != null) {
-        qualifiedAliasToLogicalNameMap.put(qualifiedPhysicalName, qualifiedLogicalName);
-      }
-    }
-    return result;
-  }
-
-  /**
-   * Releases resources associated with the given type and removes it from
-   * cache.
-   * 
-   * @param type
-   *          the type
-   */
-  public void releaseType(Type type) {
-    releaseType(type.getURI(), type.getName());
-  }
-
-  /**
-   * Releases resources associated with the given type and removes it from
-   * cache.
-   * 
-   * @param uri
-   *          The logical uri of the Type - type.getURI();
-   * @param typeName
-   *          The logical name of the Type - type.getName();
-   */
-  public void releaseType(String uri, String typeName) {
-    String qualifiedName = uri + "#" + typeName;
-    if (log.isDebugEnabled())
-      log.debug("releasing type: " + qualifiedName);
-    namespaceQualifiedNameToTypeCache.clearObject(qualifiedName);
-  }
-
-  /**
-   * Return the Type for this interfaceClass or null if not found. The
-   * interfaceClass is the Java interface that follows the SDO code generation
-   * pattern.
-   * 
-   * @param interfaceClass
-   *          is the interface for the DataObject's Type -
-   *          type.getInstanceClass();
-   * @return the Type for this interfaceClass or null if not found.
-   * @throws IllegalAccessException
-   * @throws IllegalArgumentException
-   */
-  public Type getType(Class interfaceClass) {
-    String uri = null;
-    try {
-      Field uriField = interfaceClass
-          .getDeclaredField(PlasmaDataObjectConstants.NAMESPACE_URI_FIELD_NAME);
-      uri = (String) uriField.get(null);
-    } catch (SecurityException e) {
-      throw new PlasmaDataObjectException(e);
-    } catch (NoSuchFieldException e) {
-      throw new PlasmaDataObjectException(e);
-    } catch (IllegalArgumentException e) {
-      throw new PlasmaDataObjectException(e);
-    } catch (IllegalAccessException e) {
-      throw new PlasmaDataObjectException(e);
-    }
-    return getType(uri, interfaceClass.getSimpleName());
-  }
-
-  /**
-   * Return the Type instances specified by the given uri.
-   * 
-   * @param uri
-   *          The namespace uri;
-   * @return the Type instances specified by the given uri.
-   */
-  public List<Type> getTypes(String uri) {
-    List<Type> result = new ArrayList<>();
-    List<Classifier> list = PlasmaRepository.getInstance().getClassifiers(uri);
-    for (Classifier classifier : list) {
-      if (classifier instanceof Stereotype) {
-        log.warn("ignoring stereotype: " + classifier.getName());
-        continue;
-      }
-      Type type = getType(uri, classifier.getName());
-      result.add(type);
-    }
-    return result;
   }
 
   private String getQualifiedName(PlasmaType type) {
